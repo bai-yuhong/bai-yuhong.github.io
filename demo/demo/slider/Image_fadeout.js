cf017580838b88401ecf7afe6a8da366705fe325
baiyuhong <luckybyh@qq.com>
2016-06-22 22:20:30 +0800

    Update Image_fadeout.js

--- demo/demo/slider/Image_fadeout.js
+++ demo/demo/slider/Image_fadeout.js
@@ -23,4 +23,84 @@
             })
             addEventHandle(lis2[i],'mouseout',function () {
                 addTime = setInterval(function () {
-				if(index
+				if(index<(len-1))
+				{
+					index++;
+				}
+				else
+				{
+					index=0;
+				}
+				showImag(index,lastIndex);
+					}, 1000);
+			})
+        })(i);
+    }
+    addTime = setInterval(function () {
+        if(index<(len-1))
+        {
+            index++;
+        }
+		else{
+			index=0;
+		}
+        showImag(index,lastIndex);
+
+    }, 1000);
+    function showImag(cIndex,cLastIndex) {
+        if(cIndex==cLastIndex) return;
+        fadeOut(lis1[cLastIndex]);
+		fadeIn(lis1[cIndex]);
+		lis2[cLastIndex].className="";
+        lis2[cIndex].className="hot";         
+		lastIndex=cIndex;
+		index=cIndex;
+    }
+    function fadeIn(elem)
+    {
+        setOpacity(elem,0);
+        for(var i=0;i<=20;i++)
+        {
+            (function(){
+                var pos=i*5;
+                setTimeout(function(){
+                        setOpacity(elem,pos);
+                    }
+
+                    ,i*25);
+            })(i);
+        }
+    }
+    function fadeOut(elem)
+    {
+        for(var i=0;i<=20;i++)
+        {
+            (function(){
+                var pos=100-i*5;
+                setTimeout(function(){setOpacity(elem,pos)},i*25);
+            })(i)
+        }
+    }
+    function setOpacity(elem,value)
+    {
+        if(elem.filters)
+        {
+            elem.style.filter="alpha(opacity="+value+")";
+        }
+        else
+        {
+            elem.style.opacity=value/100;
+        }
+    }//这个函数需要区分IE8以下的浏览器
+	function addEventHandle(ele,event,handle)
+	{
+			if(ele.addEventListener)
+			{
+				ele.addEventListener(event,handle);
+			}else if(ele.attachEvent)
+			{
+				ele.attachEvent("on"+event,handle);
+			}else{
+				ele["on"+event]=handle;
+			}
+	}
